d0e5458503daaae24cec83eee63c232b65164382
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 05:41:17 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -69,7 +69,6 @@ def runner
   card_total = initial_round
   while card_total < 21 do
     card_total = hit?(card_total)
-    binding.pry
     display_card_total(card_total)
   end
   end_game(card_total)
